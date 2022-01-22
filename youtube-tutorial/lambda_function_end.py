a374b57340cc2aa41484d9039e0d86096cf033db
Bhanu Pratap Singh Thakur <bpst.work@gmail.com>
2022-01-22 10:33:13 +0530

    final code

--- youtube-tutorial/lambda_function_end.py
+++ youtube-tutorial/lambda_function_end.py
@@ -13,14 +13,18 @@ def fahrenheit_to_celsius(temp):
 
 def main():
     # list of celsius temperature
-    c_temp = [17, 20, 35, 0, -2, -18]
+    c_temp = [17, 20, 35, 0, -2, -18, 68]
 
     # list of fahrenheit temperature
-    f_temp = [89, 90, 61, 20, 72, 56]
+    f_temp = [89, 90, 61, 20, 72, 56, 104.4]
 
     # TODO: Use regular functions to convert temperature
+    print(list(map(celsius_to_fahrenheit, c_temp)))
+    print(list(map(fahrenheit_to_celsius, f_temp)))
 
     # TODO: Use lambda functions to convert temperature
+    print(list(map(lambda t: (t * 9 / 5) + 32, c_temp)))
+    print(list(map(lambda t: (t - 32) * 9 / 5, f_temp)))
 
 
 if __name__ == '__main__':
